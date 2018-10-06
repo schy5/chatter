3cd96c13b353c85917d4b5b858833c9b1f8559c7
user <user@user.hsd1.mn.comcast.net>
2018-10-06 14:12:18 -0500

    separate function for html

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,16 +1,29 @@
 (ns chatter.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hiccup.page :as page]))
 
-(defroutes app-routes
-  (GET "/" []
+"(defroutes app-routes
+    (GET "/" []
        (page/html5
         [:head
          [:title "chatter"]]
         [:body
          [:h1 "Our Chat App"]]))
+  (route/not-found "Not Found"))"
+
+  (defn generate-message-view
+  "This generates the HTML for displaying messages"
+  []
+  (page/html5
+   [:head
+    [:title "chatter"]]
+   [:body
+    [:h1 "Our Chat App"]]))
+    
+ (defroutes app-routes
+  (GET "/" [] (generate-message-view))
   (route/not-found "Not Found"))
 
 (def app
