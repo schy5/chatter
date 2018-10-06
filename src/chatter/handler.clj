16593c8b4bd6de61a4a67f287b016691d9b81771
user <user@user.hsd1.mn.comcast.net>
2018-10-06 13:42:06 -0500

    change in branch view-messages

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,10 +1,16 @@
 (ns chatter.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]
+            [hiccup.page :as page]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" []
+       (page/html5
+        [:head
+         [:title "chatter"]]
+        [:body
+         [:h1 "Our Chat App"]]))
   (route/not-found "Not Found"))
 
 (def app
